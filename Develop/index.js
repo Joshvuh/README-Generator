e7efd4952363c1ad433bbb2dee21c86a06807178
Josh Lemmond <joshualemmond@gmail.com>
2022-12-10 13:40:37 -0500

    Added questions to questions array

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,17 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = ['What is the title of your project?', 'Please enter a brief description of this project', 'How do you install this project?', 'How do you use this application?', 'Contribution guidelines', 'Test instructions'];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+
+}
 
 // Function call to initialize app
 init();
