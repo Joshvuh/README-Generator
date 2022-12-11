d5a702b2e8cd382484267d3778361af1477f4900
Josh Lemmond <joshualemmond@gmail.com>
2022-12-11 13:10:44 -0500

    Updated generatemarkdown file to use readme template

--- Develop/index.js
+++ Develop/index.js
@@ -1,12 +1,61 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
 const questions = ['What is the title of your project?', 'Please enter a brief description of this project', 'How do you install this project?', 'How do you use this application?', 'Contribution guidelines', 'Test instructions'];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+const writeToFile = ({title, description, install, usage}) =>
+    `# ${title}
+
+    ## Description
+    
+    ${description}
+    
+    ## Table of Contents (Optional)
+    
+    If your README is long, add a table of contents to make it easy for users to find what they need.
+    
+    - [Installation](#installation)
+    - [Usage](#usage)
+    - [Credits](#credits)
+    - [License](#license)
+    
+    ## Installation
+    
+    ${install}
+    
+    ## Usage
+    
+    ${usage}
+
+    ## License
+    
+    The last section of a high-quality README file is the license. This lets other developers know what they can and cannot do with your project. If you need help choosing a license, refer to [https://choosealicense.com/](https://choosealicense.com/).
+    
+    ---
+    
+    🏆 The previous sections are the bare minimum, and your project will ultimately determine the content of this document. You might also want to consider adding the following sections.
+    
+    ## Badges
+    
+    ![badmath](https://img.shields.io/github/languages/top/lernantino/badmath)
+    
+    Badges aren't necessary, per se, but they demonstrate street cred. Badges let other developers know that you know what you're doing. Check out the badges hosted by [shields.io](https://shields.io/). You may not understand what they all represent now, but you will in time.
+    
+    ## Features
+    
+    If your project has a lot of features, list them here.
+    
+    ## How to Contribute
+    
+    If you created an application or package and would like other developers to contribute it, you can include guidelines for how to do so. The [Contributor Covenant](https://www.contributor-covenant.org/) is an industry standard, but you can always write your own if you'd prefer.
+    
+    ## Tests
+    
+    Go the extra mile and write tests for your application. Then provide examples on how to run them here.`;
 
 // TODO: Create a function to initialize app
 function init() {
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,19 +1,49 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function generateMarkdown(answers) {
+  return `# ${answers.title}
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+  ## Table of Contents
+  
+  - [Description](#description)
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [Contact](#contact)
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+  ## Description
+  
+  ${answers.description}
+  
+  ## Installation
+  
+  ${answers.install}
+  
+  ## Usage
+  
+  ${answers.usage}
 
-// TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+  ## License
+  
+  ![badge](https://img.shields.io/badge/license-${answers.license}-informational)
+  <br>
+  This application is using ${answers.license} license. 
+  
+  ## Contributing
+  
+  ${answers.contributing}
+  
+  ## Tests
 
+  ${answers.tests}
+
+  ## Contact
+  
+  You can reach me here:
+  <br> 
+  mailto:${answers.email} 
+  <br>
+  https://github.com/${answers.github}
 `;
 }
 
